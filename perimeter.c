acc3f3135c0c757c43a1aad5acd8eca28b776a5f
Man1shMali <manishkumarmali06393@gmail.com>
2025-07-06 20:56:55 +0530

    my first commit

---
+++ perimeter.c
@@ -0,0 +1,12 @@
+//PERIMETER OF RECTANGLE
+#include <stdio.h>
+
+int main() {
+  int l,b;
+  printf("Enter the length :");
+  scanf("%d",&l);
+  printf("Enter the Breadth :");
+  scanf("%d",&b);
+  printf("The Perimeter of the rectangle of Lenght %d and Breadth %d is %d",l,b,(2*(l+b)));
+   return 0;
+ }
